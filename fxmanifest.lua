799af1203b815a23a4c5ed54e182966b148f4a04
SharkyDev <75483199+Sharky123-Dev@users.noreply.github.com>
2021-01-28 16:34:29 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+
+author 'Sharky'
+description 'Sharky CK'
+version '1.5.0'
+
+server_scripts { 
+	'@mysql-async/lib/MySQL.lua',
+    'server/ck_sv.lua' 
+}
+
+dependencies 'mysql-async'
